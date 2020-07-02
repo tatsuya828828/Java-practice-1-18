594b9ab3373e4fe5285aa4542ac3e721dce5fb9a
tatsuya828828 <kkjy011894@gmail.com>
2020-07-02 20:07:57 +0900

    [finish]18-2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-
-import java.io.FileWriter;
+import java.io.InputStream;
+import java.net.URL;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		FileWriter fw = new FileWriter("data.txt");
-		fw.write("こんにちは");
-		fw.close();
+		URL u = new URL("https://book.impress.co.jp/");
+		InputStream is = u.openStream();
+		int i = is.read();
+		while(i != -1) {
+			char c = (char)i;
+			System.out.print(c);
+			i = is.read();
+		}
 	}
 }
